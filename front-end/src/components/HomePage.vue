53f62a5ed6795087732d9f199639b7b80106af0f
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-12-20 16:13:06 +0100

    init front-end vue.js

---
+++ front-end/src/components/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      poyo
+  </div>
+</template>
+
+<script>
+//import Vue from 'vue'
+//importiamo axios
+//import axios from 'axios';
+//costabte url
+//const API_URL = "http://localhost:8080/api/1";
+
+export default {
+  data() {
+    return {
+    
+    };
+  },
+  methods: {
+   
+  },
+    mounted() {
+     
+    }
+  
+}
+</script>
+
+<style>
+
+</style>
